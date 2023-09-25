fa3b81bf1987b265ed85d108a9f9738305e7de7d
alec-watts <alecwatts1@gmail.com>
2023-09-25 14:08:03 -0400

    f

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,19 @@
 "use strict";
 console.log('hi');
 let age = 10;
-if (age < 50)
+if (age < 50) {
     age += 10;
-console.log('old');
+    console.log('old');
+}
+let sales = [];
+let total = 0;
+let car;
+let word = 'hello';
+car = 1;
+car = 'BMW';
+let user = [1, 'Steve'];
+let stringToNum = user[0].toString();
+console.log(stringToNum);
+let blank = user[1].toLowerCase();
+console.log(blank);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-console.log('hi');
-
 let age: number = 10;
 
 if (age < 50) {
@@ -7,14 +5,22 @@ if (age < 50) {
   console.log('old')
 }
 
-let sales = [5, 10, 15];
-let total: number = 0;
-let car; // type any
-let word = 'hello';
+let sales: number[] = [];
 
+let total: number = 0;
+let word = 'hello';
+let car; // type any
 car = 1
 car = 'BMW'
 
-function render(document) {
+function render(document: any) {
   console.log(document);
 }
+
+// Tuple
+let user: [number, string] = [1, 'Steve'];
+// Tuple will automatically infer the type and show you the right functions
+let stringToNum = user[0].toString();
+console.log(stringToNum);
+let blank = user[1].toLowerCase();
+console.log(blank);
