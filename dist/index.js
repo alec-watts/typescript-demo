163f83d32c6cea7d8fae32e1cd0607ea2d84efe4
alec-watts <alecwatts1@gmail.com>
2023-09-25 14:01:29 -0400

    f

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('hi');
+let age = 10;
+if (age < 50)
+    age += 10;
+console.log('old');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+console.log('hi');
+
+let age: number = 10;
+
+if (age < 50) {
+  age += 10
+  console.log('old')
+}
+
+let sales = [5, 10, 15];
+let total: number = 0;
+let car; // type any
+let word = 'hello';
+
+car = 1
+car = 'BMW'
+
+function render(document) {
+  console.log(document);
+}
