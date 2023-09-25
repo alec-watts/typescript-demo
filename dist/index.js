d7c2a46d843b43411c717ca8c07bd8fe0cf8c0c2
alec-watts <alecwatts1@gmail.com>
2023-09-25 14:36:44 -0400

    f

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,4 @@
 "use strict";
-console.log('hi');
 let age = 10;
 if (age < 50) {
     age += 10;
@@ -7,13 +6,28 @@ if (age < 50) {
 }
 let sales = [];
 let total = 0;
-let car;
 let word = 'hello';
+let car;
 car = 1;
 car = 'BMW';
+function render(document) {
+    console.log(document);
+}
 let user = [1, 'Steve'];
 let stringToNum = user[0].toString();
 console.log(stringToNum);
 let blank = user[1].toLowerCase();
 console.log(blank);
+console.log(1, 2, 3);
+let mySize = 2;
+console.log(mySize);
+function calcululateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.3;
+    }
+    return income * 1.1;
+}
+let first = calcululateTax(100, 2021);
+let second = calcululateTax(100);
+console.log(first, second);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,44 @@ let stringToNum = user[0].toString();
 console.log(stringToNum);
 let blank = user[1].toLowerCase();
 console.log(blank);
+
+// Enums
+// If you declare enum with const, the transpiler will not generate more optimized code
+const enum Size { Small = 1, Medium, Large}
+
+console.log(Size.Small, Size.Medium, Size.Large);
+
+let mySize = Size.Medium;
+console.log(mySize);
+
+// Turn on noUnusedParameters in tsconfig.json
+// Turn on noImplicitReturns in tsconfig.json to avoid branches with undefined return
+function calcululateTax(income: number, taxYear = 2022): number {
+  // Turn on noUnusedLocals in tsconfig.json
+  // let x;
+  if (taxYear < 2022) {
+    return income * 1.3;
+  } 
+  return income * 1.1;
+}
+
+let first = calcululateTax(100, 2021);
+let second = calcululateTax(100);
+
+console.log(first, second);
+
+// Objects
+type Employee = {
+  id: number,
+  name?: string
+  retire: (date: Date) => void
+}
+
+let employee: Employee = {
+  id: 1,
+  retire: (date: Date) => {
+    console.log(date);
+  }
+}
+
+// Unions
